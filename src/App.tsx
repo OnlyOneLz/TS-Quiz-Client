cb2174b7d77da0e4947d8544ef913eb265c6c953
OnlyOneLz <elliotrobertlewis@hotmail.com>
2024-04-30 14:25:05 +0100

    finished redesign

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login/login'
 import Home from './components/home/homePage'
-import QuizPage2 from './components/quiz/quizPage2';
+import QuizPage2 from './components/quiz/quizPage';
 import QuizFinalPage from './components/quiz/quizFinalPage';
 
 function App() {
