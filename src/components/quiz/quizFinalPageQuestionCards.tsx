28054e8495955c83daf1cd15a1227432736ee9e8
OnlyOneLz <elliotrobertlewis@hotmail.com>
2024-05-07 09:35:38 +0100

    refactored modal

--- src/components/quiz/quizFinalPageQuestionCards.tsx
+++ src/components/quiz/quizFinalPageQuestionCards.tsx
@@ -14,11 +14,13 @@ interface QuestionCardProps {
 export const QuizFinalPageQuestionCards: React.FC<QuestionCardProps> = ({ question, userAnswer, checkAnswer, correctAnswer }) => {
     const [modalOpen, setModalOpen] = useState<any>(false);
     return (
-        <div>
-            <div className="final-question-cards" key={question.id}>
-                <div className={`tick-cross-box ${checkAnswer ? 'correct-answer-background' : 'wrong-answer-background'}`}></div>
-                <p className="final-question">{question.question}</p>
-                <button className="open-question-modal" onClick={() => setModalOpen(!modalOpen)}></button>
+        <>
+            <div className={`final-question-cards`} key={question.id}>
+                <div className="first-line-q-a">
+                    <div className={`tick-cross-box ${checkAnswer ? 'correct-answer-background' : 'wrong-answer-background'}`}></div>
+                    <p className="final-question">{question.question}</p>
+                    <button className="open-question-modal" onClick={() => setModalOpen(!modalOpen)}></button>
+                </div>
                 <div className="question-modal" style={modalOpen ? { display: 'grid' } : { display: 'none' }}>
                     <p>
                         <strong>Your Selected Answer:</strong> {userAnswer}
@@ -30,10 +32,13 @@ export const QuizFinalPageQuestionCards: React.FC<QuestionCardProps> = ({ questi
                             <strong>Correct Answer:</strong> {correctAnswer}
                         </p>
                     )}
-                    <p>Explanation- {question.explanation}</p>
+                    <p>
+                        <strong>Explanation: </strong>
+                        {question.explanation}
+                    </p>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
